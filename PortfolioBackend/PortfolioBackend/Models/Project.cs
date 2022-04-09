cd6dd0edbb6a2e6adeb8f53ac49988a24b171b57
Kyle Hennessy <kyle-hennessy@hotmail.com>
2022-04-09 16:10:53 +0100

    Added more properties to the Project model

--- PortfolioBackend/PortfolioBackend/Models/Project.cs
+++ PortfolioBackend/PortfolioBackend/Models/Project.cs
@@ -12,11 +12,26 @@ namespace PortfolioBackend.Models
         [BsonElement("title")]
         public string Title { get; set; } = string.Empty;
 
+        [BsonElement("summary")]
+        public string Summary { get; set; } = string.Empty;
+
         [BsonElement("description")]
         public string Description { get; set; } = string.Empty;
 
         [BsonElement("thumbnailUrl")]
         public string ThumbnailUrl { get; set; } = string.Empty;
+
+        [BsonElement("previewUrl")]
+        public string PreviewUrl { get; set; } = string.Empty;
+
+        [BsonElement("demoUrl")]
+        public string DemoUrl { get; set; } = string.Empty;
+
+        public List<string> DetailImagesUrl { get; set; } = new List<string>();
+
+        [BsonElement("sourceCodeUrl")]
+        public string SourceCodeUrl { get; set; } = string.Empty;
+
 
         public List<Skill> SkillsUsed { get; set; } = new List<Skill>();
 
